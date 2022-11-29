ae99797415a937dc0f9e04cdb023c042eac198c4
407 <407@academicos.cesde.edu.co>
2022-11-29 16:41:03 -0500

    proyectofinal

---
+++ src/Home/Home.js
@@ -0,0 +1,73 @@
+import './Home.css'
+export function Home(){
+    return(
+        <>
+        <section>
+            <div class="banner">
+               
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-12 col-md-8">
+                    <h2>ALCOLIRICOS</h2>
+                        <p>“Todo lo bueno tarda”, dice una de las canciones de su último álbum, Aranjuez, presentado el sábado anterior en Medellín. Hace unos meses, en menos de dos horas las boletas se agotaron para esta presentación en el Teatro Carlos Vieco: el público esperaba con ansiedad este concierto, tardío y aplazado por los tiempos pandémicos.
+
+                        Después de ver la respuesta de la gente, se alistaron para una segunda presentación, el mismo sábado, a las 5. Con luz de luna entre nubes oscuras y gente emocionada más allá de cualquier fila o espera, aparecieron en el escenario Kaztro, Gambeta y Fazeta, integrantes de Alcolirykoz, una banda de rap que nació en las calles de Aranjuez, en Medellín.
+
+                        </p>
+                    </div>
+                    
+                    <div class="col-12 col-md-4">
+                   
+                    <div class="row">
+                    
+                    <div class= "col-3">
+      
+                    <i class="bi bi-music-note-list fs-1"></i>
+                    </div>
+                    <div class= "col-3">
+      
+                    <p>lorem10
+
+                    </p>
+                    </div>
+                    
+                    </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+        <section>
+        <div class="container-fluid">
+            <div class= "row">
+            <div class="col-12 col-md-4">
+                <img src='https://firebasestorage.googleapis.com/v0/b/spotydqq.appspot.com/o/carousel2.png?alt=media&token=42aacde5-b6ab-4134-b69f-0abcb74d1f87' alt='' class="img-fluid w-100"/>
+           
+            </div>
+
+            <div class="col-12 col-md-4">
+                <img src='https://firebasestorage.googleapis.com/v0/b/spotydqq.appspot.com/o/carousel22.jpg?alt=media&token=99e53119-ed0f-426c-bdf1-1e7f7c83ce59' alt='' class="img-fluid w-100"/>
+           
+            </div>
+
+            <div class="col-12 col-md-4">
+                <img src='https://firebasestorage.googleapis.com/v0/b/spotydqq.appspot.com/o/carousel3.jpg?alt=media&token=b7a6de8f-0e36-4aa6-b9e5-df9b4afd7a60' alt='' class="img-fluid w-100"/>
+           
+            </div>
+            </div>
+            </div>
+
+        
+        </section>
+        
+        </>
+    )
+}
