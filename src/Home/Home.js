b944912ffc01889394d645ccd0e6c5a5a17bbbe6
DavidQ2001 <davidestebanq65@gmail.com>
2022-12-06 12:10:36 -0500

    proyectoreact

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -15,40 +15,89 @@ export function Home(){
                 <div class="row">
                     
                     <div class="col-12 col-md-8">
-                    <h2>ALCOLIRICOS</h2>
+                    <h2>ALCOLIRIKOZ</h2>
                         <p>“Todo lo bueno tarda”, dice una de las canciones de su último álbum, Aranjuez, presentado el sábado anterior en Medellín. Hace unos meses, en menos de dos horas las boletas se agotaron para esta presentación en el Teatro Carlos Vieco: el público esperaba con ansiedad este concierto, tardío y aplazado por los tiempos pandémicos.
 
                         Después de ver la respuesta de la gente, se alistaron para una segunda presentación, el mismo sábado, a las 5. Con luz de luna entre nubes oscuras y gente emocionada más allá de cualquier fila o espera, aparecieron en el escenario Kaztro, Gambeta y Fazeta, integrantes de Alcolirykoz, una banda de rap que nació en las calles de Aranjuez, en Medellín.
 
                         </p>
+
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotydqq.appspot.com/o/LLEGADA.jpg?alt=media&token=5d78f0a9-59ff-4b4f-8a3b-ecbadf21e7d0" alt="" class="img-fluid w-100" />
                     </div>
                     
-                    <div class="col-12 col-md-4">
+                    <div class="col-12 col-md-4 tarjeta ">
                    
-                    <div class="row">
+                        <div class="row">
                     
-                    <div class= "col-3">
+                             <div class= "col-3 col-md-4">
       
-                    <i class="bi bi-music-note-list fs-1"></i>
-                    </div>
-                    <div class= "col-3">
+                                <i class="bi bi-music-note-list fs-1"></i>
+                            
+                            </div>
+                        <div class= "col-3 col-md-4 mt-2">
       
-                    <p>lorem10
-
-                    </p>
+                            <p>lorem10</p>
                     </div>
                     
-                    </div>
-
-                    </div>
-
+                    
                 </div>
 
+                <div class="row">
+                    
+                    <div class= "col-3 col-md-4 mt-2">
+
+                       <i class="bi bi-music-note-list fs-1"></i>
+                   
+                   </div>
+               <div class= "col-3 col-md-4 mt-2 text-center">
+
+                        <p>25 DIC</p>
+                        <p>25 DIC</p>
+                   
+           </div>
+           
+           
+       </div>
+
+       <div class="row">
+                    
+                    <div class= "col-3 col-md-4 mt-2">
+
+                       <i class="bi bi-music-note-list fs-1"></i>
+                   
+                   </div>
+               <div class= "col-3 col-md-4 mt-2 ">
+
+                   <p>lorem10</p>
+           </div>
+           
+           
+       </div>
+
+       <div class="row">
+                    
+                    <div class= "col-3 col-md-4 mt-2">
+
+                       <i class="bi bi-music-note-list fs-1"></i>
+                   
+                   </div>
+               <div class= "col-3 col-md-4 mt-2">
+
+                   <p>lorem10</p>
+           </div>
+           
+           
+       </div>
+
             </div>
+
+         </div>
+
+    </div>
         </section>
 
         <section>
-        <div class="container-fluid">
+        <div class="container-fluid mt-4">
             <div class= "row">
             <div class="col-12 col-md-4">
                 <img src='https://firebasestorage.googleapis.com/v0/b/spotydqq.appspot.com/o/carousel2.png?alt=media&token=42aacde5-b6ab-4134-b69f-0abcb74d1f87' alt='' class="img-fluid w-100"/>
--- src/shared/Menu/Menu.js
+++ src/shared/Menu/Menu.js
@@ -7,7 +7,7 @@ export function Menu(){
 return(
 <nav class="navbar navbar-expand-lg menu navbar-dark">
       <div class="container-fluid">
-                <Link class="navbar-brand" to="/">Alcoliricos</Link>
+                <Link class="navbar-brand" to="/">Alcolirikoz</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
           </button>
